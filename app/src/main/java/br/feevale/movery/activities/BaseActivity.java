799c6b7a3c0b9d79de550b704e0fb4484b920fc7
Rodrigo Dagnese <dagnese.rodrigo@gmail.com>
2017-06-08 11:15:52 -0300

    estrutura inicial login

---
+++ app/src/main/java/br/feevale/movery/activities/BaseActivity.java
@@ -0,0 +1,19 @@
+package br.feevale.movery.activities;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by rodrigo.dagnese on 08/06/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+  private ProgressDialog progressDialog;
+  private AlertDialog alertDialog;
+
+  public void longToast(String message) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+  }
+}
---
+++ app/src/main/java/br/feevale/movery/services/AuthenticationService.java
@@ -0,0 +1,40 @@
+package br.feevale.movery.services;
+
+import android.content.Context;
+
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationAPIClient;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.callback.BaseCallback;
+import com.auth0.android.result.Credentials;
+
+/**
+ * Created by rodrigo.dagnese on 08/06/2017.
+ */
+
+public class AuthenticationService {
+  private Auth0 account;
+  private AuthenticationAPIClient authentication;
+
+  public AuthenticationService(Context context) {
+    account = new Auth0(context);
+    authentication = new AuthenticationAPIClient(account);
+  }
+
+  public void login(String name, String password) {
+    authentication.login(name, password)
+      .start(new BaseCallback<Credentials, AuthenticationException>() {
+        @Override
+        public void onSuccess(Credentials payload) {
+
+        }
+
+        @Override
+        public void onFailure(AuthenticationException error) {
+
+        }
+      });
+  }
+
+  public void logout() {}
+}
