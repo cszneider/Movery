3b5a9e402f861603d50bce65deacf073344a298c
dagneserodrigo <dagnese.rodrigo@gmail.com>
2017-06-24 15:21:40 -0300

    validações nos campos de sign in

--- app/src/main/java/br/feevale/movery/services/AuthenticationService.java
+++ app/src/main/java/br/feevale/movery/services/AuthenticationService.java
@@ -1,6 +1,7 @@
 package br.feevale.movery.services;
 
 import android.content.Context;
+import android.content.Intent;
 
 import com.auth0.android.Auth0;
 import com.auth0.android.authentication.AuthenticationAPIClient;
@@ -8,33 +9,38 @@ import com.auth0.android.authentication.AuthenticationException;
 import com.auth0.android.callback.BaseCallback;
 import com.auth0.android.result.Credentials;
 
+import br.feevale.movery.MainActivity;
+
 /**
  * Created by rodrigo.dagnese on 08/06/2017.
  */
 
 public class AuthenticationService {
-  private Auth0 account;
-  private AuthenticationAPIClient authentication;
+    private Auth0 account;
+    private AuthenticationAPIClient authentication;
+    private Context context;
 
-  public AuthenticationService(Context context) {
-    account = new Auth0(context);
-    authentication = new AuthenticationAPIClient(account);
-  }
+    public AuthenticationService(Context activityContext) {
+        account = new Auth0(activityContext);
+        authentication = new AuthenticationAPIClient(account);
+        context = activityContext;
+    }
 
-  public void signIn(String name, String password) {
-    authentication.login(name, password)
-      .start(new BaseCallback<Credentials, AuthenticationException>() {
-        @Override
-        public void onSuccess(Credentials payload) {
+    public void signIn(String email, String password) {
+        authentication.login(email, password)
+                .start(new BaseCallback<Credentials, AuthenticationException>() {
+                    @Override
+                    public void onSuccess(Credentials payload) {
+                        Intent intent = new Intent(context, MainActivity.class);
+                    }
 
-        }
+                    @Override
+                    public void onFailure(AuthenticationException error) {
 
-        @Override
-        public void onFailure(AuthenticationException error) {
+                    }
+                });
+    }
 
-        }
-      });
-  }
-
-  public void logout() {}
+    public void logout() {
+    }
 }
---
+++ app/src/main/java/br/feevale/movery/utils/Constants.java
@@ -0,0 +1,10 @@
+package br.feevale.movery.utils;
+
+/**
+ * Created by dagneserodrigo on 6/24/17.
+ */
+
+public class Constants {
+    public static final String INVALID_EMAIL = "Verifique seu E-mail.";
+    public static final String INVALID_PASSWORD = "Senha inválida";
+}
